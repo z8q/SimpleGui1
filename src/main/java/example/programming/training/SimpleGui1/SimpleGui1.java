64bc4bd33f200a08aa04c9b29eccfdca7da3bbed
Kirill <ponkirill@gmail.com>
2021-02-01 01:03:02 +0300

    Added ActionListener

--- src/main/java/example/programming/training/SimpleGui1/SimpleGui1.java
+++ src/main/java/example/programming/training/SimpleGui1/SimpleGui1.java
@@ -2,16 +2,30 @@ package example.programming.training.SimpleGui1;
 
 
 import javax.swing.*;
+import java.awt.event.*;
 
-public class SimpleGui1 {
+public class SimpleGui1 implements ActionListener{
+	
+	JButton button;
+	
 	public static void main (String[] args) {
+		SimpleGui1 gui = new SimpleGui1();
+		gui.go();
+	}
+	
+	public void go() {
 		JFrame frame = new JFrame();
-		JButton button = new JButton("Click me");
+		button = new JButton("Click me");
+		
+		button.addActionListener(this);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		frame.getContentPane().add(button);
 		frame.setSize(300,300);
 		frame.setVisible(true);
+	}
+	public void actionPerformed(ActionEvent event) {
+		button.setText("I've been clicked!");
 	}
 }
